a882721ed42cf7a03e1d7de1cea86e934157c7d6
suraj <suraj.upadhyay@xebia.com>
2022-10-20 17:54:17 +0530

    Updates

---
+++ precache-manifest.0785793afd8d72ed2f193424a73acb12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd67a33df5042ac2a115",
+    "url": "/d3-sunburst-chart/static/css/main.853274ca.chunk.css"
+  },
+  {
+    "revision": "fd67a33df5042ac2a115",
+    "url": "/d3-sunburst-chart/static/js/main.fd67a33d.chunk.js"
+  },
+  {
+    "revision": "38ce9586cb9645472e5a",
+    "url": "/d3-sunburst-chart/static/js/1.38ce9586.chunk.js"
+  },
+  {
+    "revision": "4665ce8871ab0ad481b3",
+    "url": "/d3-sunburst-chart/static/js/2.4665ce88.chunk.js"
+  },
+  {
+    "revision": "90589f55fcaf65fa5b97",
+    "url": "/d3-sunburst-chart/static/js/runtime~main.90589f55.js"
+  },
+  {
+    "revision": "03094388a4806e729494aeecebf7dda3",
+    "url": "/d3-sunburst-chart/index.html"
+  }
+];
